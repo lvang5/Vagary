e6563ad6bd9a29e7423fab55394ad57e55424756
Lais Vang <vang.lais05@gmail.com>
2018-09-18 13:49:21 -0500

    added input value

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -4,33 +4,43 @@ import React, { Component } from 'react';
 class LandingPage extends Component {
   constructor() {
     super()
-    this.state = { location: [] }
+    this.state = { location: '' }
   }
 
   handleClick = () => {
-    this.props.history.push('view');
-    console.log('clicked');
-
+    // this.props.history.push('view');
+    //here is where we do our axios request
   }
 
-
-
-
+  handleInput = (event) => {
+    //this takes in the value input
+    this.setState({
+      location: event.target.value
+     })
+     
+  }
 
   render() {
     return (
+      
       <div>
         Welcome to the landing page
-         <input placeholder="e.g. City, State, Zipcode" />
+         <input placeholder="e.g. City, State, Zipcode" onBlur={this.handleChange}/>
         <button onClick={this.handleClick}>Submit</button>
         {/* Find image for backgroud */}
         <div>
           <div className="container-div">
-            some text
+          <h3>Mission Statement</h3>
+          Vagary is an application that offers affordable rental pricing for vehicles to 
+          tourist/locals wherever they plan to visit. Anyone can access the site and view 
+          the different vehicles that are available to rent. In order for a user to be a 
+          renter or complete a rent, that user will have to create an account with the application 
+          to be able to checkout/add their vehicle.  
+          
            {/* Mission Statement */}
           </div>
           <div>
-            some text
+            some text for how
   
           </div>
           <div>
